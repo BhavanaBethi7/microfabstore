64367afb5d349435b4f68fc3f5fb23198d1ed815
Bethi Bhavana <bhavanabethi7@gmail.com>
2025-12-13 15:33:17 -0600

    Fix JSX entry for Vite (use main.jsx)

--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-// src/index.js
-
-
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-// Create React root and render the App component
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
